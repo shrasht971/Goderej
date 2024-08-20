72ce93e7878b258a52cf68919afa9d9f1d4fae2f
shrasht971 <94127570+shrasht971@users.noreply.github.com>
2024-08-20 22:29:45 +0530

    Second commit.

--- src/Compnents/MainSection.jsx
+++ src/Compnents/MainSection.jsx
@@ -8,20 +8,20 @@ import godrej3 from '../assets/image/godrej3.jpeg';
 import godrej4 from '../assets/image/godrej4.jpeg';
 import godrej5 from '../assets/image/godrej5.jpeg';
 
-
-
 const MainSection = () => {
   return (
     <div className="flex flex-col md:flex-row h-[70vh] lg:h-[48vh] md:h-[48vh] custom-lg:h-[54vh] 3xl:h-[47vh]">
-      <div className="w-full md:w-2/6 p-4 flex flex-col justify-center items-center relative bg-white ">
-        <img 
-          src={Image} 
-          alt="Explore" 
-          className="w-full h-2/3 md:h-full object-cover shadow-lg" 
-        />
-        <button className="absolute bottom-4 md:bottom-10 px-4 md:px-6 py-2 md:py-3 bg-blue-600 text-white rounded-md shadow-md hover:bg-blue-700 transition duration-300 ease-in-out">
-          Explore
-        </button>
+      <div className="w-full md:w-2/6 p-4 flex flex-col justify-center items-center relative bg-white">
+        <div className="relative w-full h-2/3 md:h-full">
+          <img 
+            src={Image} 
+            alt="Explore" 
+            className="w-full h-full object-cover shadow-lg" 
+          />
+          <button className="absolute bottom-4 left-1/2 transform -translate-x-1/2 px-4 md:px-6 py-2 md:py-1 border-2 border-white text-white rounded-md shadow-md hover:bg-gray-700 transition duration-300 ease-in-out">
+            Explore
+          </button>
+        </div>
       </div>
       <div className="w-full md:w-4/6 p-2">
         <Carousel 
